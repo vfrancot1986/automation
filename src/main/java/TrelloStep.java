857bca74536089cd799846023b521b6833780d2d
Alex Villanova <alexsrvillanova@gmail.com>
2018-11-24 14:07:42 -0200

    bcxvcxv

---
+++ src/main/java/TrelloStep.java
@@ -0,0 +1,41 @@
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.E;
+import cucumber.api.java.pt.Entao;
+import cucumber.api.java.pt.Quando;
+
+public class TrelloStep {
+    @Dado("^que esteja logado no trello$")
+    public void queEstejaLogadoNoTrello() throws Throwable {
+        System.out.println("queEstejaLogadoNoTrello");
+    }
+
+    @E("^acesse o board$")
+    public void acesseOBoard() throws Throwable {
+        System.out.println("acesseOBoard");
+    }
+
+    @Quando("^crio um card com o nome \"([^\"]*)\"$")
+    public void crioUmCardComONome(String arg0) throws Throwable {
+        System.out.println("crioUmCardComONome");
+    }
+
+    @E("^comento \"([^\"]*)\"$")
+    public void comento(String arg0) throws Throwable {
+        System.out.println("comento");
+    }
+
+    @Entao("^o card deve estar na lista$")
+    public void oCardDeveEstarNaLista() throws Throwable {
+        System.out.println("oCardDeveEstarNaLista");
+    }
+
+    @Quando("^excluo o card$")
+    public void excluoOCard() throws Throwable {
+        System.out.println("excluoOCard");
+    }
+
+    @Entao("^o card não existe mais$")
+    public void oCardNãoExisteMais() throws Throwable {
+        System.out.println("oCardNãoExisteMais");
+    }
+}
